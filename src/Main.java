f53ec8a03e1d213b6a8d3b20d828354dc17a1637
vikash9738822 <vikash.kuamr@cashfree.com>
2025-02-05 14:49:11 +0530

    first commit of submain

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 
 
+
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,10 +12,22 @@ public class Main {
         System.out.println("Hello and welcome4");
         System.out.println("Hello and welcome5");
         System.out.println("Hello and welcome6");
-        
+
+
+
+
         for (int i = 1; i <= 5; i++) {
 
             System.out.println("i = " + i);
         }
+
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome1");
+        System.out.println("Hello and welcome2");
+        System.out.println("Hello and welcome3");
+        System.out.println("Hello and welcome4");
+        System.out.println("Hello and welcome5");
+        System.out.println("Hello and welcome6");
+
     }
 }
